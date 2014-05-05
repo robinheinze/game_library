20fd1e2c2234213507a557c74711076b4eb832ca
Robin Miller <student@epicodus.com>
2014-05-05 11:08:39 -0700

    add styles and images

--- game_bucket_ember/js/app.js
+++ game_bucket_ember/js/app.js
@@ -14,22 +14,28 @@ App.IndexRoute = Ember.Route.extend({
   }
 });
 
+//CONTROLLERS
+
+
 //MODELS
 
 App.ApplicationAdapter = DS.FixtureAdapter.extend();
 App.Game = DS.Model.extend({
   title: DS.attr('string'),
+  description: DS.attr('string'),
+  image: DS.attr('string'),
   year: DS.attr('number'),
   console: DS.attr('string'),
   company: DS.attr('string'),
   isCompleted: DS.attr('boolean')
 });
 
-
 App.Game.FIXTURES = [
   {
     id: 1,
+    description: 'A historical fiction action-adventure open world stealth video game that follows Desmond as he lives the life of an assassin during the Third Crusade through the eyes of his ancestor Altair.',
     title: "Assassin's Creed",
+    image: 'http://fc06.deviantart.net/fs71/f/2011/202/f/8/assassin__s_creed_game_icon_by_wolfangraul-d418pbu.png',
     year: 2007,
     console: 'Xbox',
     company: 'Ubisoft',
@@ -38,6 +44,8 @@ App.Game.FIXTURES = [
   {
     id: 2,
     title: "Portal",
+    description: 'A first-person puzzle-platform game that follows a test subject wielding an Aperture Science Handheld Portal Device around Aperture Science laboratories shooting portals and teleporting through them to solve puzzles.',
+    image: 'http://imgttg.com/user/avatar/892922_85114.png',
     year: 2007,
     console: 'Xbox',
     company: 'Valve',
@@ -46,6 +54,8 @@ App.Game.FIXTURES = [
   {
     id: 3,
     title: 'Halo 4',
+    description: 'An online first-person shooter video game set in a futuristic science fiction setting where players are responsible for defending humanity from the Covenant (aliens).',
+    image: 'http://d1vr6n66ssr06c.cloudfront.net/wp-content/themes/egmnowv3/images/icons/Halo-4.png',
     year: 2012,
     console: 'Xbox',
     company: '343 Industries',
